fe8926df25e8221f23c34469f69dd410d3873b61
Fernando Pinto <fschultepinto@gmail.com>
2022-09-23 21:01:41 -0300

    Resolução do problema

--- Problem-5/script.js
+++ Problem-5/script.js
@@ -1,7 +1,9 @@
-let itemList;
+let itemList = [];
 
 for(let item = 0 ; item <10 ; item++) {
 
-  let itemList = prompt("Digite o nome do item " + (item + 1))
-
+  let itemName = prompt("Digite o nome do item " + (item + 1))
+  itemList[item] = itemName
 }
+
+alert(itemList)
