3ac6ce317d5868ed1b2a331d70d2e068df096a5e
Fernando Pinto <fschultepinto@gmail.com>
2022-09-26 12:23:45 -0300

    Atualização do problema

--- Problem-4/script.js
+++ Problem-4/script.js
@@ -1,16 +1,15 @@
 let student = prompt("Qual o nome do aluno?")
 let n1 = prompt("Qual a nota da primeira prova?")
 let n2 = prompt("Qual a nota da segunda prova?")
-let n3 = prompt("Qual a nota da terceira prova?")
 
-let avg = (Number(n1) + Number(n2) + Number(n3)) / 3
+let avg = (Number(n1) + Number(n2)) / 2
 
 avg = avg.toFixed(2)
 
-if (avg >= 6){
-  alert("Parabéns, " + student + "! Você está aprovado!")
+if (avg >= 7){
   alert("A sua média é: " + avg)
+  alert("Parabéns, " + student + "! \nVocê está aprovado(a) no concurso!")
 } else {
-  alert("Você está reprovado, " + student + "! Mas, nem tudo está perdido, estude bastante para o exame que você aprovará!")
   alert("A sua média é: " + avg)
+  alert("Você está reprovado, " + student + "! \nContinue estudando que a sua vez chegará!")
 }
